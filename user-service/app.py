db92d289743bd968df83822c620f655dc86bb06f
Gaurav Bhardwaj <36173630+GauravBhardwaj10@users.noreply.github.com>
2025-08-06 19:54:16 -0600

    Update app.py
    Port update

--- user-service/app.py
+++ user-service/app.py
@@ -11,4 +11,4 @@ def get_user(username):
     return jsonify({"username": username, "email": f"{username}@example.com"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8081)
